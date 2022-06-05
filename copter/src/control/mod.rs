0f7b94790b860804b676646e05ee5c542768c630
Matt Hunzinger <matthunz2@gmail.com>
2022-06-05 02:43:44 -0400

    Refactor Controller struct

--- copter/src/control/mod.rs
+++ copter/src/control/mod.rs
@@ -11,9 +11,8 @@ mod lateral_pos;
 pub use lateral_pos::LateralPositionController;
 
 mod yaw;
-use nalgebra::{Vector3, Vector2};
+use nalgebra::{Vector2, Vector3};
 pub use yaw::YawController;
-
 
 pub struct Controller {
     pub altitude: AltitudeController,
@@ -24,47 +23,68 @@ pub struct Controller {
 }
 
 impl Controller {
-  /// Calculate the desired roll, pitch, yaw, and thrust moment commands in Newtons*meters.
-  pub fn position_control(
-    &self,
-    local_position_cmd: Vector3<f32>,
-    local_velocity_cmd: Vector3<f32>,
-    local_position: Vector3<f32>,
-    local_velocity: Vector3<f32>,
-    attitude: Vector3<f32>,
-    gyro: Vector3<f32>,
-    acceleration_ff: Vector2<f32>,
-    moment_of_inertia: Vector3<f32>
-) -> (Vector3<f32>, f32) {
-    let acceleration_cmd = self.lateral_position.lateral_position_control(
-        Vector2::new(local_position_cmd[0], local_position_cmd[1]),
-        Vector2::new(local_velocity_cmd[0], local_velocity_cmd[1]),
-        Vector2::new(local_position[0], local_position[1]),
-        Vector2::new(local_velocity[0], local_velocity[1]),
-        acceleration_ff,
-    );
-    let thrust_cmd = self.altitude.acceleration(
-        -local_position_cmd[2],
-        -local_velocity_cmd[2],
-        -local_position[2],
-        -local_velocity[2],
-        attitude,
-        9.81,
-    );
+    /// Calculate the desired roll, pitch and yaw torque (in Nm) with the thrust acceleration (in m/s^2).
+    pub fn position_control(
+        &self,
+        local_position_cmd: Vector3<f32>,
+        local_velocity_cmd: Vector3<f32>,
+        local_position: Vector3<f32>,
+        local_velocity: Vector3<f32>,
+        attitude: Vector3<f32>,
+        gyro: Vector3<f32>,
+        moment_of_inertia: Vector3<f32>,
+    ) -> (Vector3<f32>, f32) {
+        self.position_control_with_feed_forward(
+            local_position_cmd,
+            local_velocity_cmd,
+            local_position,
+            local_velocity,
+            attitude,
+            gyro,
+            moment_of_inertia,
+            Vector2::zeros(),
+        )
+    }
 
-    let roll_pitch_rate_cmd =
-        self.attitude
-            .roll_pitch_control(acceleration_cmd, attitude, thrust_cmd);
-    let yaw_rate_cmd = self
-        .yaw
-        .yaw_control(local_position_cmd[2], attitude[2]);
+    /// Calculate the desired roll, pitch and yaw torque (in Nm) with the thrust acceleration (in m/s^2).
+    pub fn position_control_with_feed_forward(
+        &self,
+        local_position_cmd: Vector3<f32>,
+        local_velocity_cmd: Vector3<f32>,
+        local_position: Vector3<f32>,
+        local_velocity: Vector3<f32>,
+        attitude: Vector3<f32>,
+        gyro: Vector3<f32>,
+        moment_of_inertia: Vector3<f32>,
+        acceleration_ff: Vector2<f32>,
+    ) -> (Vector3<f32>, f32) {
+        let acceleration_cmd = self.lateral_position.lateral_position_control(
+            Vector2::new(local_position_cmd[0], local_position_cmd[1]),
+            Vector2::new(local_velocity_cmd[0], local_velocity_cmd[1]),
+            Vector2::new(local_position[0], local_position[1]),
+            Vector2::new(local_velocity[0], local_velocity[1]),
+            acceleration_ff,
+        );
+        let thrust_acceleration = self.altitude.acceleration(
+            -local_position_cmd[2],
+            -local_velocity_cmd[2],
+            -local_position[2],
+            -local_velocity[2],
+            attitude,
+            9.81,
+        );
 
-    let body_rate_cmd =
-        Vector3::new(roll_pitch_rate_cmd[0], roll_pitch_rate_cmd[1], yaw_rate_cmd);
-    let moment = self
-        .body_rate
-        .body_rate_control(body_rate_cmd, gyro, moment_of_inertia);
+        let roll_pitch_rate_cmd =
+            self.attitude
+                .roll_pitch_control(acceleration_cmd, attitude, thrust_acceleration);
+        let yaw_rate_cmd = self.yaw.yaw_control(local_position_cmd[2], attitude[2]);
 
-        (moment, thrust_cmd)
+        let body_rate_cmd =
+            Vector3::new(roll_pitch_rate_cmd[0], roll_pitch_rate_cmd[1], yaw_rate_cmd);
+        let moment = self
+            .body_rate
+            .body_rate_control(body_rate_cmd, gyro, moment_of_inertia);
 
-}}
+        (moment, thrust_acceleration)
+    }
+}
