c94c81941e5c24293726b29a4ecdf28d4484fc25
Matt Hunzinger <matthunz2@gmail.com>
2022-06-06 13:12:10 -0400

    Publish v0.3.0

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,20 @@
+//! # embedded-flight
+//! A `#![no_std]` flight software library for embedded rust
+//!
+//! # Generic components
+//! [`Scheduler`] real-time scheduler to run tasks at desired frequencies. 
+//! 
+//! [`hal`] contains the hardware abstraction layer.
+//! 
+//! 
+//! 
+//! # Multi-copter components
+//! [`Copter`] a high level position controller for a multi-copter
+//! (see [`QuadCopter`] for a quad-motor implementation)
+//! 
+//! [`control`](copter::control) contains the low level flight controllers.
+//! 
+//! [`motor`](copter::control::motor) contains the low level motor control.
 pub mod copter;
 pub use copter::{Copter, QuadCopter};
 
@@ -5,3 +22,4 @@ pub mod hal;
 pub use hal::{Sensors, ESC};
 
 pub mod scheduler;
+pub use scheduler::Scheduler;
--- src/scheduler/task.rs
+++ src/scheduler/task.rs
@@ -1,3 +1,53 @@
+//! # embedded-flight
+//! A `#![no_std]` flight software library for embedded rust
+//!
+//! # Examples
+//!
+//! For more check out the
+//! [basic](https://github.com/matthunz/embedded-flight/blob/main/examples/basic.rs) or
+//! [quad](https://github.com/matthunz/embedded-flight/tree/main/examples/quad)
+//! examples on GitHub.
+//!
+//! Create and run a [`MultiCopter`] from a motor matrix, inertial sensor, clock, and frequency (in hz).
+//! ```ignore
+//! use embedded_flight::copter::{MultiCopter, multi_copter_tasks};
+//! use embedded_flight::motors::MotorMatrix;
+//!
+//! // Create a quad-copter motor matrix from 4 ESCs
+//! let motor_matrix = MotorMatrix::quad(ESC(0), ESC(1), ESC(2), ESC(3));
+//!
+//! // Initialize the tasks to run from the scheduler
+//! // By default this will stabilize the attitude
+//! let mut tasks = multi_copter_tasks();
+//!
+//! // Create the quad-copter
+//! let mut copter = MultiCopter::new(motors, imu, &mut tasks, clock, 400);
+//!
+//! // Run the tasks in the scheduler and output to the motors in a loop
+//! drone.run()?
+//! ```
+//!
+//! Use the lower level [`MultiCopterAttitudeController`](control::attitude::MultiCopterAttitudeController) to get the motor output for a desired attitude:
+//! ```
+//! use embedded_flight::control::attitude::MultiCopterAttitudeController;
+//! use nalgebra::{Vector3, Quaternion};
+//!
+//! let mut controller = MultiCopterAttitudeController::default();
+//!
+//! // Input the desired attitude and angular velocity with the current attitude
+//! controller.attitude_controller.input(
+//!     Quaternion::default(),
+//!     Vector3::default(),
+//!     Quaternion::default(),
+//! );
+//!
+//! // Output the control to the motors with the current gyroscope data.
+//! let output = controller.motor_output(Vector3::default(), 1);
+//! dbg!(output);
+//! ```
+
+#![no_std]
+
 use super::Error;
 use embedded_time::duration::Microseconds;
 
