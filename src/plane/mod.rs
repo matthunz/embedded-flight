38ab4a80bb27a4c20093eae75d6524a9e3fff8e9
Matt Hunzinger <matthunz2@gmail.com>
2022-06-07 01:46:16 -0400

    Create plane LongitudinalControl struct

--- src/plane/mod.rs
+++ src/plane/mod.rs
@@ -2,9 +2,21 @@ use core::f32::consts::PI;
 
 use pid_controller::{error, P};
 
+pub struct LongitudinalControl {
+    pub p: P<f32>,
+    pub pitch_rate_gain: f32,
+}
+
+impl LongitudinalControl {
+    /// Calculate the elevator command required to acheive the target pitch
+    pub fn pitch_control(&self, pitch: f32, pitch_rate: f32, pitch_cmd: f32) -> f32 {
+        self.p.control(pitch_cmd, pitch) - self.pitch_rate_gain * pitch_rate
+    }
+}
+
 pub struct LateralControl {
     pub roll_rate_gain: f32,
-    pub p: P<f32>,
+    pub roll: P<f32>,
     pub yaw: P<f32>,
     pub yaw_integrator: f32,
     pub max_roll: f32,
@@ -13,12 +25,12 @@ pub struct LateralControl {
 
 impl LateralControl {
     /// Calculate the aileron (in [-1, 1]) to command a roll (in radians) from the current roll.
-    pub fn roll_attitude_hold_loop(&self, roll_cmd: f32, roll: f32, roll_rate: f32) -> f32 {
-        self.p.control(roll_cmd, roll) - self.roll_rate_gain * roll_rate
+    pub fn roll_control(&self, roll_cmd: f32, roll: f32, roll_rate: f32) -> f32 {
+        self.roll.control(roll_cmd, roll) - self.roll_rate_gain * roll_rate
     }
 
-    /// Calculate the commanded roll angle from the course/yaw angle
-    pub fn yaw_hold_loop(&mut self, yaw_cmd: f32, yaw: f32, dt: f32, roll_ff: f32) -> f32 {
+    /// Calculate the commanded roll angle (in radians) from the course/yaw angle
+    pub fn yaw_control(&mut self, yaw_cmd: f32, yaw: f32, dt: f32, roll_ff: f32) -> f32 {
         // Calculate the error in yaw
         let mut yaw_error = error(yaw_cmd, yaw);
         while yaw_error < PI {
