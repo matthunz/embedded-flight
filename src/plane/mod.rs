1d33e8750d0ee1b944fece2573dbd66b34789271
Matt Hunzinger <matthunz2@gmail.com>
2022-06-07 02:28:17 -0400

    Add airspeed control to plane

--- src/plane/mod.rs
+++ src/plane/mod.rs
@@ -4,11 +4,66 @@ use pid_controller::{error, P};
 
 pub struct LongitudinalControl {
     pub p: P<f32>,
+    pub speed: P<f32>,
+    pub pitch_speed: P<f32>,
     pub pitch_rate_gain: f32,
+    pub speed_integrator: f32,
+    pub climb_speed_integrator: f32,
+    pub speed_i_gain: f32,
+    pub min_throttle: f32,
+    pub max_throttle: f32,
+    pub max_pitch_cmd: f32,
+    pub pitch_speed_i_gain: f32,
 }
 
 impl LongitudinalControl {
-    /// Calculate the elevator command required to acheive the target pitch
+    /// Calculate the throttle command (in [-1, 1]) required to command the target airspeed (in m/s).
+    /// Uses the current airspeed with the time-step `dt` (in seconds).
+    pub fn throttle(&mut self, airspeed: f32, airspeed_cmd: f32, dt: f32, throttle_ff: f32) -> f32 {
+        let speed_error = error(airspeed_cmd, airspeed);
+        self.speed_integrator += speed_error * dt;
+
+        let throttle_cmd_unsaturated = self.speed.control_with_error(speed_error)
+            + self.speed_i_gain * self.speed_integrator
+            + throttle_ff;
+
+        let throttle_cmd = throttle_cmd_unsaturated
+            .max(self.min_throttle)
+            .min(self.max_throttle);
+
+        if self.speed_i_gain != 0. {
+            self.speed_integrator += self.speed_integrator
+                + dt / self.speed_i_gain * (throttle_cmd - throttle_cmd_unsaturated)
+        }
+
+        throttle_cmd
+    }
+
+    /// Calculate the pitch command (in radians) required to maintain the commanded airspeed (in m/s);
+    /// Uses the current airspeed and time-step `dt` (in seconds).
+    pub fn airspeed_pitch(&mut self, airspeed: f32, airspeed_cmd: f32, dt: f32) -> f32 {
+        let airspeed_error = error(airspeed_cmd, airspeed);
+        self.climb_speed_integrator += airspeed_error * dt;
+
+        let pitch_cmd_unsat = self.pitch_speed.control_with_error(airspeed_error)
+            + self.pitch_speed_i_gain * self.climb_speed_integrator;
+
+        let pitch_cmd = if pitch_cmd_unsat.abs() > self.max_pitch_cmd {
+            pitch_cmd_unsat.signum() * self.max_pitch_cmd
+        } else {
+            pitch_cmd_unsat
+        };
+
+        // Anti wind-up
+        if self.pitch_speed_i_gain != 0. {
+            self.climb_speed_integrator +=
+                dt / self.pitch_speed_i_gain * (pitch_cmd - pitch_cmd_unsat);
+        }
+
+        pitch_cmd
+    }
+
+    /// Calculate the elevator command (in [-1, 1]) required to achieve the target pitch.
     pub fn pitch_control(&self, pitch: f32, pitch_rate: f32, pitch_cmd: f32) -> f32 {
         self.p.control(pitch_cmd, pitch) - self.pitch_rate_gain * pitch_rate
     }
